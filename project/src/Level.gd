1e47b707e8aa4d3efa7f7c030790c63db320b351
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-10-03 10:33:14 -0400

    Translate world rather than use second camera

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-onready var _camera : Camera2D = $Camera2D
 onready var _Alien := preload("res://src/Alien.tscn")
 onready var _aliens := $Aliens
 onready var _game_over_fade_in := $GameOverFadeIn
 
 func _ready():
-	_camera.current = true
 	for i in range(0,5):
 		var alien = _Alien.instance()
 		alien.rotate(TAU * i / 5)
